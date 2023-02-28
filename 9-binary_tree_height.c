b83d2697140ea3ce650e6eb286659dde0597b154
Meacha1 <meachattd@gmail.com>
2023-02-28 04:14:58 -0800

    9_edited

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,19 +6,25 @@
  * @tree: pointer to the root of tree to be measured
  * Return: the size of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree) {
-    size_t left_height, right_height;
 
-    if (tree == NULL) {
-        return 0;
-    }
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0, height_right = 0;
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    if (left_height > right_height) {
-        return left_height + 1;
-    } else {
-        return right_height + 1;
-    }
+	if (tree->left != NULL) 
+	{
+		height_left = binary_tree_height(tree->left) + 1;
+	}
+
+	if (tree->right != NULL) 
+	{
+		height_right = binary_tree_height(tree->right) + 1;
+	}
+
+	return (height_left > height_right) ? height_left : height_right;
 }
