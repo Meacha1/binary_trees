8f93abbebba94e977193f4f0eb13f5eb309da1d1
Meacha1 <meachattd@gmail.com>
2023-02-28 11:45:43 -0800

    12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Entry point
+ *
+ * @tree: the node to be checked
+ * Return: the number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+		return (2);
+	if (tree->left != NULL && tree->right == NULL)
+		return (1);
+	if (tree->right != NULL && tree->left == NULL)
+		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+}
