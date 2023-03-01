194b2fab2e665df878f4dcaf5bfc66814f972f38
Meacha1 <meachattd@gmail.com>
2023-03-01 00:11:34 -0800

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left_height - right_height);
 }
+
+size_t binary_tree_height(const binary_tree_t *tree) {
+    size_t height_left = 0, height_right = 0;
+
+    if (tree == NULL) {
+        return 0;
+    }
+
+    if (tree->left != NULL) {
+        height_left = binary_tree_height(tree->left) + 1;
+    }
+
+    if (tree->right != NULL) {
+        height_right = binary_tree_height(tree->right) + 1;
+    }
+
+    return (height_left > height_right) ? height_left : height_right;
+}
