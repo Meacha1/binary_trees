b218be0268cd9526f10d967639db25ffebd5899f
Meacha1 <meachattd@gmail.com>
2023-03-01 00:32:42 -0800

    full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,16 +9,16 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-	    return (0);
+		return (0);
 
-    /* if the node is a leaf node, it is full */
-    if (tree->left == NULL && tree->right == NULL)
-	    return (1);
+	/* if the node is a leaf node, it is full */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    /* if the node has both children, recursively check both subtrees */
-    if (tree->left && tree->right)
-	    return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	/* if the node has both children, recursively check both subtrees */
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-    /* if the node has only one child, it is not full */
-    return (0);
+	/* if the node has only one child, it is not full */
+	return (0);
 }
