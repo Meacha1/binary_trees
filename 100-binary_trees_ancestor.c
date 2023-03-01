848aae1676cdd39a0e8041f28f16c498bbaac94d
Meacha1 <meachattd@gmail.com>
2023-03-01 08:04:39 -0800

    ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,16 +8,16 @@
  * Return: a pointer to the common ancestor
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    if (first == NULL || second == NULL)
-        return NULL;
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first == second || first == second->parent)
+		return ((binary_tree_t *)first);
 
-    if (first == second || first == second->parent)
-        return (binary_tree_t *)first;
+	if (second == first->parent)
+		return ((binary_tree_t *)second);
 
-    if (second == first->parent)
-        return (binary_tree_t *)second;
-
-    return binary_trees_ancestor(first->parent, second->parent);
+	return (binary_trees_ancestor(first->parent, second->parent));
 }
