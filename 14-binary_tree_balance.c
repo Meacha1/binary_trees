5e3287acd39d801a4e2ae23cceaad53aa436f074
Meacha1 <meachattd@gmail.com>
2023-03-01 00:17:19 -0800

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,20 +20,31 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (left_height - right_height);
 }
 
-size_t binary_tree_height(const binary_tree_t *tree) {
-    size_t height_left = 0, height_right = 0;
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: pointer to the root of tree to be measured
+ * Return: the size of the tree
+ */
 
-    if (tree == NULL) {
-        return 0;
-    }
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0, height_right = 0;
 
-    if (tree->left != NULL) {
-        height_left = binary_tree_height(tree->left) + 1;
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    if (tree->right != NULL) {
-        height_right = binary_tree_height(tree->right) + 1;
-    }
+	if (tree->left != NULL)
+	{
+		height_left = binary_tree_height(tree->left) + 1;
+	}
 
-    return (height_left > height_right) ? height_left : height_right;
+	if (tree->right != NULL)
+	{
+		height_right = binary_tree_height(tree->right) + 1;
+	}
+
+	return ((height_left > height_right) ? height_left : height_right);
 }
