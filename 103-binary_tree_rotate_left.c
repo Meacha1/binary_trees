e655bc7822b4a1f3220a69e3fadce8adf896426f
Meacha1 <meachattd@gmail.com>
2023-03-02 12:37:31 -0800

    rotation

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,27 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_root = NULL;
+	binary_tree_t *new_root = NULL;
 
-    if (!tree)
-        return NULL;
+	if (!tree)
+		return (NULL);
 
-    if (tree->right)
-    {
-        new_root = tree->right;
-        tree->right = new_root->left;
-        if (new_root->left)
-            new_root->left->parent = tree;
-        new_root->parent = tree->parent;
-        if (!tree->parent)
-            new_root->parent = NULL;
-        else if (tree->parent->left == tree)
-            tree->parent->left = new_root;
-        else
-            tree->parent->right = new_root;
-        new_root->left = tree;
-        tree->parent = new_root;
-    }
+	if (tree->right)
+	{
+		new_root = tree->right;
+		tree->right = new_root->left;
+		if (new_root->left)
+			new_root->left->parent = tree;
+		new_root->parent = tree->parent;
+		if (!tree->parent)
+			new_root->parent = NULL;
+		else if (tree->parent->left == tree)
+			tree->parent->left = new_root;
+		else
+			tree->parent->right = new_root;
+		new_root->left = tree;
+		tree->parent = new_root;
+	}
 
-    return new_root;
+	return (new_root);
 }
