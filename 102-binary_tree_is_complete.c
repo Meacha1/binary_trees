2756d344e5899abd9a06635a12098e1593453ed0
Meacha1 <meachattd@gmail.com>
2023-03-02 12:12:01 -0800

    complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -8,16 +8,14 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (binary_tree_balance(tree) == 0)
-        return (1);
-
-    return (0);
+	if (binary_tree_balance(tree) == 0)
+		return (1);
+	return (0);
 }
 
-#include "binary_trees.h"
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
@@ -28,15 +26,15 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        int left_height, right_height;
+	int left_height, right_height;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-        return (left_height - right_height);
+	return (left_height - right_height);
 }
 
 /**
@@ -48,22 +46,22 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t height_left = 0, height_right = 0;
+	size_t height_left = 0, height_right = 0;
 
-        if (tree == NULL)
-        {
-                return (0);
-        }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-        if (tree->left != NULL)
-        {
-                height_left = binary_tree_height(tree->left) + 1;
-        }
+	if (tree->left != NULL)
+	{
+		height_left = binary_tree_height(tree->left) + 1;
+	}
 
-        if (tree->right != NULL)
-        {
-                height_right = binary_tree_height(tree->right) + 1;
-        }
+	if (tree->right != NULL)
+	{
+		height_right = binary_tree_height(tree->right) + 1;
+	}
 
-        return ((height_left > height_right) ? height_left : height_right);
+	return ((height_left > height_right) ? height_left : height_right);
 }
