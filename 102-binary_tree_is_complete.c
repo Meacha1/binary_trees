f098ee071e854f242deafe100517199169933d5c
Meacha1 <meachattd@gmail.com>
2023-03-02 02:44:19 -0800

    102-complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,55 +1,69 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_complete - checks if a binary tree is complete
- * @tree: pointer to the root node of the tree to check
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree to check
  *
  * Return: 1 if the tree is complete, 0 otherwise
  */
-
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    queue_t *queue = NULL;
-    int flag = 0;
-
     if (tree == NULL)
         return (0);
 
-    queue = queue_create();
-    if (queue == NULL)
-        return (0);
+    if (binary_tree_balance(tree) == 0)
+        return (1);
 
-    enqueue(queue, (void *)tree);
+    return (0);
+}
 
-    while (!is_empty(queue))
-    {
-        binary_tree_t *node = (binary_tree_t *)dequeue(queue);
+#include "binary_trees.h"
 
-        if (node->left)
-        {
-            if (flag)
-            {
-                queue_free(queue);
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance factor of the binary tree, 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+        int left_height, right_height;
+
+        if (tree == NULL)
                 return (0);
-            }
-            enqueue(queue, (void *)node->left);
-        }
-        else
-            flag = 1;
 
-        if (node->right)
+        left_height = binary_tree_height(tree->left);
+        right_height = binary_tree_height(tree->right);
+
+        return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: pointer to the root of tree to be measured
+ * Return: the size of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t height_left = 0, height_right = 0;
+
+        if (tree == NULL)
         {
-            if (flag)
-            {
-                queue_free(queue);
                 return (0);
-            }
-            enqueue(queue, (void *)node->right);
         }
-        else
-            flag = 1;
-    }
 
-    queue_free(queue);
-    return (1);
+        if (tree->left != NULL)
+        {
+                height_left = binary_tree_height(tree->left) + 1;
+        }
+
+        if (tree->right != NULL)
+        {
+                height_right = binary_tree_height(tree->right) + 1;
+        }
+
+        return ((height_left > height_right) ? height_left : height_right);
 }
